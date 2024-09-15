72b28f4c1c179de9054388356b3f59bfb9912cac
jarrett <jarrett@Ubuntu.myguest.virtualbox.org>
2024-09-15 14:17:55 -0700

    fixing image issue 3

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -50,10 +50,10 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
 fi
 
 #make image
-cd "$OUTDIR"/linux-stable
-mkdir -p ${OUTDIR}/Image
+cd "$OUTDIR"
+mkdir -p Image
 echo "Adding the Image in outdir"
-cp arch/${ARCH}/boot/Image/ Image/
+cp linux-stable/arch/${ARCH}/boot/Image/ Image/
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
