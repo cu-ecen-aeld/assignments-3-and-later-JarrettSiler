000f3a84ad26d4347736683ecbbf6220fb212f39
jarrett <jarrett@Ubuntu.myguest.virtualbox.org>
2024-10-16 09:11:05 -0700

    assignment 5 pt 2 initial commit

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,16 @@
+case "$1" in
+    start)
+        echo "Starting aesdsocket"
+        #start command
+        start-stop-daemon --start --oknodo --make-pidfile --pidfile /var/run/aesdsocket.pid --chdir /home/jarrett/projects/assignments-3-and-later-JarrettSiler/server --exec ./aesdsocket -- -d; then
+        ;;
+    stop)
+        echo "Stopping aesdsocket"
+        start-stop-daemon --stop --signal SIGTERM --pidfile /var/run/aesdsocket.pid
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
+
+exit 0
